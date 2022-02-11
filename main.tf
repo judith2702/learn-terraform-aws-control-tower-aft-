190261ac2b86b900549cd8be05d7c2224c93e045
judith2702 <87411824+judith2702@users.noreply.github.com>
2022-02-11 15:47:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,13 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+module "aft" {
+  source = "github.com/aws-ia/terraform-aws-control_tower_account_factory"
+  # Required Vars
+  ct_management_account_id    = "660479874226"
+  log_archive_account_id      = "865548136817"
+  audit_account_id            = "201306435926"
+  aft_management_account_id   = "893995042829"
+  ct_home_region              = "us-east-1"
+  tf_backend_secondary_region = "us-west-2"
 }
-
-
-# Configure the AWS Provider
-provider "aws" {
-  region = "us-east-1"
-  profile = "MYAWS"
- }
